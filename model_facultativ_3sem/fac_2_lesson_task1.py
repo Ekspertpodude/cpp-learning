30e517ec64f6e35729c13e7c70f01566d6898781
Ekspert <borisenko.rs@phystech.edu>
2026-09-17 14:32:15 +0300

    Lesson 1 task 5(fac)

--- model_facultativ_3sem/fac_2_lesson_task1.py
+++ model_facultativ_3sem/fac_2_lesson_task1.py
@@ -1,1 +1,16 @@
-print ('Hello world')
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+phi_0 = np.pi* 0
+a = 1
+b = 3
+
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 4))
+t = np.linspace(-100, 1 * np.pi, 1000)
+print (t)
+f_1 = np.sin(phi_0 + a*t)
+f2 = np.cos (b*t)
+print(f_1,f2)
+ax[0,0].plot(f2,f_1, label = 'Синус первый')
+
+plt.show()
