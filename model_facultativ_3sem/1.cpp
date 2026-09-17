4918762bd34301e6dad0c03cedfb6c86da6d038f
Ekspert <borisenko.rs@phystech.edu>
2026-09-17 14:56:35 +0300

    Lesson2 task 6(fac)

--- model_facultativ_3sem/1.cpp
+++ model_facultativ_3sem/1.cpp
@@ -3,22 +3,20 @@
 
   
 
-int main(){
+int main( int argc, char *argv[]){
 
- std:: cout << "Hello, World!" << std::endl;
- int n = 0;
-
-std:: cout << " Print number of numbers:" << std::endl;
-std::cin >> n;
 std::ofstream out;
-
+if (argc < 2){
+    std::cout << "Incorrect input; Please write  number n" << std::endl;
+}
+int n = 0;
 out.open("Hello_text.txt");
-
+n  = std::stoi(argv[1]);
 if (out.is_open()){
 
 std:: cout << "Hello, World!" << std::endl;
 
-for (int i = 1; i<=n; i++){
+for (int i = 1; i <= n; i++){
 
 out <<' ' <<i;
 
