7805a396359155707d75e96ad3fb24ad08e5070b
Ekspert <borisenko.rs@phystech.edu>
2026-09-18 12:32:06 +0300

    Lesson 2(fac) task1 - Lissagu's figure in python

--- model_facultativ_3sem/fac_2_lesson_task1.py
+++ model_facultativ_3sem/fac_2_lesson_task1.py
@@ -1,16 +1,32 @@
 import matplotlib.pyplot as plt
 import math
 import numpy as np
-phi_0 = np.pi* 0
-a = 1
+a = 2
 b = 3
-
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 4))
-t = np.linspace(-100, 1 * np.pi, 1000)
+c = 1
+d = 2 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+ax[0].set_aspect('equal')
+ax[1].set_aspect('equal')
+t = np.linspace(0, 4 * np.pi, 1000)
 print (t)
-f_1 = np.sin(phi_0 + a*t)
-f2 = np.cos (b*t)
-print(f_1,f2)
-ax[0,0].plot(f2,f_1, label = 'Синус первый')
+f1 = np.sin( a*t)
+f2 = np.sin (b*t)
+f3 = np.sin( c*t)
+f4 = np.sin(d*t)
+print(f1,f2)
+ax[0].plot(f1,f2, label = 'Фигура 1')
+ax[0].set_title(rf'Фигура 1: $ \frac{{F_x}}{{F_y}} = \frac {{{a}}}{{{b}}}$', fontsize=14, pad=10) # Заголовок
+ax[0].set_xlabel('F_x', fontsize=11)                 # Подпись оси X
+ax[0].set_ylabel('F_y', fontsize=11)   
+
+
+ax[1].plot(f3 , f4 , label = 'Фигура 2')
+ax[1].set_title(rf'Фигура 2: $ \frac{{F_x}}{{F_y}} = \frac {{{c}}}{{{d}}}$', fontsize=14, pad=10) # Заголовок
+ax[1].set_xlabel('F_x', fontsize=11)                 # Подпись оси X
+ax[1].set_ylabel('F_y', fontsize=11)  
+ax[0].grid()
+ax[1].grid()
+
 
 plt.show()
